29e2ec0bd1dad40740c357ec66d5664c6ee4968d
Alan-Miller <alan.ashman@gmail.com>
2016-11-16 13:25:50 -0700

    prediction solution

---
+++ intermediate/prediction.js
@@ -0,0 +1,13 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  THE PREDICTION
+  https://www.codewars.com/kata/the-prediction/train/javascript
+
+  Description:
+  How good are your prediction skills? Can you find our magic number?
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function predict() {
+	Math.random = function() {
+  }
+	return 0;
+}
