8c82b5dc58ab3a49b67c54aab59b3616cf26de08
Alan-Miller <alan.ashman@gmail.com>
2016-11-19 14:47:19 -0700

    is-a-prime and zip-with

---
+++ intermediate/is-a-prime.js
@@ -0,0 +1,11 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  IS A PRIME?
+  https://www.codewars.com/kata/is-a-prime/train/javascript
+
+  Description:
+  Is a Prime?
+
+  Just your usual prime kata ... with some unknown restrictions ... Good luck!
+  This kata requires a lot of guess work so try and try again. :)
+  Note: You may not have any loops
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
