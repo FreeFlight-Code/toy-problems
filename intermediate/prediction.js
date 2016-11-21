6a26bc7fcbc537e0ce2b76a91c936b907aedbd3e
Alan-Miller <alan.ashman@gmail.com>
2016-11-21 15:43:43 -0700

    remove duplicates

--- intermediate/prediction.js
+++
@@ -1,7 +0,0 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
-  THE PREDICTION
-  https://www.codewars.com/kata/the-prediction/train/javascript
-
-  Description:
-  How good are your prediction skills? Can you find our magic number?
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
