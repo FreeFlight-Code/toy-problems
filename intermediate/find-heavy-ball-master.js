595ae3e6a6a711585049130a19693a62076219ce
Alan-Miller <alan.ashman@gmail.com>
2016-11-21 00:54:04 -0700

    heavy ball master solution

---
+++ intermediate/find-heavy-ball-master.js
@@ -0,0 +1,62 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  FIND HEAVY BALL — CONQUEROR
+  https://www.codewars.com/kata/find-heavy-ball-level-conqueror/train/javascript
+
+  Description:
+
+  There are 8 balls numbered from 0 to 7. Seven of them have the same weight. One is heavier. Your task is to find it's number.
+
+  Your function findBall will receive single argument - scales object. The scales object contains an internally stored array of 8 elements (indexes 0-7), each having the same value except one, which is greater. It also has a public method named getWeight(left, right) which takes two arrays of indexes and returns -1, 0, or 1 based on the accumulation of the values found at the indexes passed are heavier, equal, or lighter.
+
+  getWeight returns:
+
+  -1 if left pan is heavier
+
+  1 if right pan is heavier
+
+  0 if both pans weigh the same
+
+  Examples of scales.getWeight() usage:
+
+  scales.getWeight([3], [7]) returns -1 if ball 3 is heavier than ball 7, 1 if ball 7 is heavier, or 0 i these balls have the same weight.
+
+  scales.getWeight([3, 4], [5, 2]) returns -1 if weight of balls 3 and 4 is heavier than weight of balls 5 and 2 etc.
+
+  So where's the catch, you may ask. Well - the scales is very old. You can use it only TWICE before the scale breaks.
+
+  Note - Use scales.get_weight() in Python version.
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function findBall(scales) {
+
+  var leftPan = [0, 1, 2];
+  var rightPan = [3, 4, 5];
+  var w = scales.getWeight(leftPan, rightPan);
+
+  if (w === -1) {
+      leftPan = [0];
+      rightPan = [1];
+      w = scales.getWeight(leftPan, rightPan);
+			if (w === -1) return 0;
+			else if (w === 1) return 1;
+      else return 2;
+  }
+
+  else if (w === 1) {
+  	  leftPan = [3];
+      rightPan = [4];
+      w = scales.getWeight(leftPan, rightPan);
+			if (w === -1) return 3;
+      else if (w === 1) return 4;
+      else return 5;
+  }
+
+	else {
+  	leftPan = [6];
+    rightPan = [7];
+    w = scales.getWeight(leftPan, rightPan);
+    if (w === -1) return 6;
+    else return 7;
+  }
+}
