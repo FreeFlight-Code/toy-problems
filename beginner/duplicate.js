9ef12f9da527ae720891ebb44af9e5a404c51706
Alan-Miller <alan.ashman@gmail.com>
2016-11-21 15:29:43 -0700

    duplicate solution

---
+++ beginner/duplicate.js
@@ -0,0 +1,26 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  DUPLICATE
+
+  Input is an array.
+  Output should be the same array, duplicated.
+
+  Examples:
+    Input: [2, 4, 6, 8]
+    Output: [2, 4, 6, 8, 2, 4, 6, 8]
+
+    Input: ['cat', 'alligator', 'unicorn']
+    Output: ['cat', 'alligator', 'unicorn', 'cat', 'alligator', 'unicorn']
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function duplicate(array) {
+  // newArray is copy (not just alias) of array, allowing original array to remain unchanged
+  var newArray = Array(array.length).fill(null).map(function(c, i) {
+    return array[i];
+  });
+  // .map method duplicates the array, pushing values to back of array
+  newArray.map(function(current) {
+    return newArray.push(current);
+  });
+  // return the newArray
+  return newArray;
+}
