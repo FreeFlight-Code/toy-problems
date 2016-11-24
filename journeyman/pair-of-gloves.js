db4321b9069573f2a9d794942d88dddf84c473df
Alan-Miller <alan.ashman@gmail.com>
2016-11-24 02:13:43 -0800

    gloves

---
+++ journeyman/pair-of-gloves.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  PAIR OF GLOVES
+  https://www.codewars.com/kata/pair-of-gloves/train/javascript
+
+  Pair of gloves
+
+  Winter is comming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+  A pair of gloves is constituted of two gloves of the same color.
+
+  You are given an array containing the color of each glove.
+
+  You must return the number of pair you can constitute.
+
+  You must not change the input array.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
