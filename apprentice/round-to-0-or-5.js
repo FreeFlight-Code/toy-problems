5e37e850b59ad975c4adf4f8b69992250208c5f2
Alan-Miller <alan.ashman@gmail.com>
2016-11-25 01:12:20 -0800

    round to 0 or 5

---
+++ apprentice/round-to-0-or-5.js
@@ -0,0 +1,15 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  ROUND TO NEAREST 0 OR 5
+  https://www.codewars.com/kata/round-to-nearest-0-or-5/train/javascript
+
+  Description:
+
+  Given an array of numbers, return an array, with each member of input array rounded to a nearest number, divisible by 5.
+
+  For example:
+
+  roundToFive([34.5, 56.2, 11, 13]);
+
+  should return
+  [35, 55, 10, 15]
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
