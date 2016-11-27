1769faf9beabadc6a0fca7a988571482fdb22022
Alan-Miller <alan.ashman@gmail.com>
2016-11-27 10:53:15 -0800

    photocopier

---
+++ apprentice/office3-photocopier.js
@@ -0,0 +1,15 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  THE OFFICE III - BROKEN PHOTOCOPIER
+  https://www.codewars.com/kata/the-office-iii-broken-photocopier/train/javascript
+
+  The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+  Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+  Given a string of binary, return the version the photocopier gives you as a string.
+
+  The Office I - Outed
+  The Office II - Boredeom Score
+  The Office IV - Find a Meeting Room
+  The Office V - Find a Chair
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
