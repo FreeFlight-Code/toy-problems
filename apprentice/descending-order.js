02b3f6ed564082d920a7c753efb563b86b7f25d8
Alan-Miller <alan.ashman@gmail.com>
2016-11-27 09:47:39 -0800

    yay

--- apprentice/descending-order.js
+++ apprentice/descending-order.js
@@ -12,3 +12,7 @@
 
   Input: 1254859723 Output: 9875543221
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function descendingOrder(n){
+	return Number(n.toString().split('').sort().reverse().join(''));
+}
