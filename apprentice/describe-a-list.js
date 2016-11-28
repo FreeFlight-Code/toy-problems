6876697c6f7bfb9f90a90562bc1f637b79a35dda
Alan-Miller <alan.ashman@gmail.com>
2016-11-28 03:14:28 -0800

    describe a list solution

---
+++ apprentice/describe-a-list.js
@@ -0,0 +1,10 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  DESCRIBE A LIST
+  https://www.codewars.com/kata/describe-a-list/train/javascript
+
+  Write function describeList which tells if the list is empty or contains only one element or more.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function describeList(x) {
+	return !x.length ? 'empty' : x.length === 1 ? 'singleton' : 'longer';
+}
