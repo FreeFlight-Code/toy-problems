cde1d5ae19c97f9980d23a152718ad54db01ce4d
Alan-Miller <alan.ashman@gmail.com>
2016-11-27 16:52:55 -0800

    acronym buster solution

---
+++ journeyman/acronym-buster.js
@@ -0,0 +1,42 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  ACRONYM BUSTER
+  https://www.codewars.com/kata/acronym-buster/train/javascript
+
+  Laura really hates people using acronyms in her office and wants to force her colleagues to remove all acronyms before emailing her. She wants you to build a system that will edit out all known acronyms or else will notify the sender if unknown acronyms are present.
+
+  Any combination of three or more letters in upper case will be considered an acronym. Acronyms will not be combined with lowercase letters, such as in the case of 'KPIs'. They will be kept isolated as a word/words within a string.
+
+  For any string:
+
+  All instances of 'KPI' must become "key performance indicators"
+  All instances of 'EOD' must become "the end of the day"
+  All instances of 'TBD' must become "to be decided"
+  All instances of 'WAH' must become "work at home"
+  All instances of 'IAM' must become "in a meeting"
+  All instances of 'OOO' must become "out of office"
+  All instances of 'NRN' must become "no reply necessary"
+  All instances of 'CTA' must become "call to action"
+  All instances of 'SWOT' must become "strengths, weaknesses, opportunities and threats"
+
+  If there are any unknown acronyms in the string, Laura wants you to return only the message:
+  '[acronym] is an acronym. I do not like acronyms. Please remove them from your email.'
+
+  So if the acronym in question was 'BRB', you would return the string:
+
+  'BRB is an acronym. I do not like acronyms. Please remove them from your email.'
+
+  If there is more than one unknown acronym in the string, return only the first in your answer.
+
+  If all acronyms can be replaced with full words according to the above, however, return only the altered string.
+
+  If this is the case, ensure that sentences still start with capital letters. '!' or '?' will not be used.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function acronymBuster(string){
+  string = string.replace(/KPI/g, 'key performance indicators').replace(/EOP/g, 'the end of the day').replace(/TBD/g, 'to be decided').replace(/WAH/g, 'work at home').replace(/IAM/g, 'in a meeting').replace(/OOO/g, 'out of office').replace(/NRN/g, 'no reply necessary').replace(/CTA/g, 'call to action').replace(/SWOT/g, 'strengths, weaknesses, opportunities and threats').replace(/^[a-z]/, function(startOfSentence) {return startOfSentence.toUpperCase();}).replace(/\.\s[a-z]/g, function(v) {return v.toUpperCase();});
+
+  var acronym = string.match(/[A-Z]{3,}/);
+
+  if (acronym) return acronym[0] + ' is an acronym. I do not like acronyms. Please remove them from your email.';
+  else return string;
+}
