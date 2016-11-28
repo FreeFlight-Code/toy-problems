a78c8fc1c80203d670cbaee49f9010ad9f05c751
Alan-Miller <alan.ashman@gmail.com>
2016-11-28 03:13:45 -0800

    describe a list

---
+++ apprentice/describe-a-list.js
@@ -0,0 +1,6 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  DESCRIBE A LIST
+  https://www.codewars.com/kata/describe-a-list/train/javascript
+
+  Write function describeList which tells if the list is empty or contains only one element or more.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
