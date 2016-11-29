d994cfa846c92558248dd383089e9c5e892295e0
Alan-Miller <alan.ashman@gmail.com>
2016-11-28 23:51:55 -0800

    squash bugs

---
+++ apprentice/squash-the-bugs.js
@@ -0,0 +1,10 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SQUASH THE BUGS
+  https://www.codewars.com/kata/squash-the-bugs/train/javascript
+
+  Description:
+
+  Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+  There will only be one 'longest' word.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
