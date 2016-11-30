768e313d112686e7ad08fb193ce5876447135f3b
Alan-Miller <alan.ashman@gmail.com>
2016-11-29 21:56:13 -0800

    convert number to reversed array solution

---
+++ apprentice/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,21 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  CONVERT NUMBER TO REVERSED ARRAY OF DIGITS
+  https://www.codewars.com/kata/convert-number-to-reversed-array-of-digits/train/javascript
+
+  Description:
+
+  Convert number to reversed array of digits
+
+  Given a random number:
+
+  C#: long;
+  C++: unsigned long;
+  You have to return the digits of this number within an array in reverse order.
+  Example:
+
+  348597 => [7,9,5,8,4,3]
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function digitize(n) {
+	return n.toString().split('').reverse().map(function(c) {return Number(c)})
+}
