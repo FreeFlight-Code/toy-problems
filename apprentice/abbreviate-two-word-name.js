70661dce8ef5d757d243a126e555f99f65b4c9f5
Alan-Miller <alan.ashman@gmail.com>
2016-11-29 22:30:34 -0800

    abbreviate two word name

---
+++ apprentice/abbreviate-two-word-name.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  ABBREVIATE A TWO WORD NAME
+  https://www.codewars.com/kata/abbreviate-a-two-word-name/train/javascript
+
+  Description:
+
+  Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+  The output should be two letters with a dot separating them.
+
+  It should look like this:
+
+  Sam Harris => S.H
+
+  Patrick Feeney => P.F
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
