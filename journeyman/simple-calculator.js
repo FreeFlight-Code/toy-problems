5dafcdbff137a3093cb42b8106ef4bbdf95f465f
Alan-Miller <alan.ashman@gmail.com>
2016-12-01 23:55:38 -0700

    simple calculator

---
+++ journeyman/simple-calculator.js
@@ -0,0 +1,17 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SIMPLE CALCULATOR
+  https://www.codewars.com/kata/simple-calculator/train/javascript
+
+  You are required to create a simple calculator that returns the result of addition ,subtraction , multiplication and
+  division of two numbers . example
+
+  calculator(1,2,"+"); //=> result will be 3
+  if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+  example
+
+  calculator(1,2,"&"); //=> result will be "unknown value"
+  calculator(1,"k","*"); //=> result will be "unknown value"
+
+  Good luck
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
