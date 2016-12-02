8a7a734a750147501bc0d563369a21b27e0baa2e
Alan-Miller <alan.ashman@gmail.com>
2016-12-01 23:31:41 -0700

    sum of positive

---
+++ apprentice/sum-of-positive.js
@@ -0,0 +1,12 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SUM OF POSITIVE
+  https://www.codewars.com/kata/sum-of-positive/train/javascript
+
+  Description:
+
+  You get an array of numbers, return the sum of all of the positives ones.
+
+  Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+  Note: array may be empty, in this case return 0.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
