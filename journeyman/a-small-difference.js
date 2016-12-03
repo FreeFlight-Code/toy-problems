6959e3d7b75f6b36f24f421ca498f31966bd3d79
Alan-Miller <alan.ashman@gmail.com>
2016-12-03 02:22:07 -0700

    small difference

--- journeyman/a-small-difference.js
+++ journeyman/a-small-difference.js
@@ -15,24 +15,3 @@
   tower / tower -> false
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-function oneCharDifference(s1, s2) {
-  if (Math.abs(s1.length - s2.length) > 1) return false;
-
-  var first = s1.length >= s2.length ? s1.split('') : s2.split('');
-  var second = s1.length < s2.length ? s1.split('') : s2.split('');
-
-  if (first.length !== second.length) {
-    var indexOfDiff = first.map(function(c, i) {
-      if (c === second[i]) return i;
-    });
-    indexOfDiff = indexOfDiff.indexOf(undefined);
-    second.splice(indexOfDiff, 0, '?');
-  }
-
-  var diff = second.filter(function(c, i, a) {
-    return c !== first[i];
-  });
-
-  return diff.length === 1;
-}
