8edf9007751e13f09ec2c9c7a350791d36007376
Alan-Miller <alan.ashman@gmail.com>
2016-12-05 02:37:51 -0700

    sloppy censorship

---
+++ journeyman/sloppy-censorship.js
@@ -0,0 +1,14 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SLOPPY CENSORSHIP
+  https://www.codewars.com/kata/sloppy-censorship/train/javascript
+
+  As the owner of a down-at-heel publishing firm, you're excited to have acquired the rights to minor celebrity's bawdy memoirs.
+
+  You need to get them printed before the tabloids get wind of them, but you're worried they might be libellous.
+
+  Write a function which acts on a string to obscure any word beginning with an uppercase letter by replacing each subsequent character with an underscore _.
+
+  The initial capital letter should remain intact, as should all other uppercase letters in a word not normally capitalised. Therefore, anything in camelCase would not be censored, but brand names with InCapping (YouTube) will (Y__T__).
+
+  As this is shoddy rush-job, you don't need to account for the capital letters which start a sentence, nor should you bother preventing punctuation like periods and possessives ('s, 're) from being replaced.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
