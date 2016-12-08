c11cb990a7a07a7ca3ed0b857f893c84b0e0292b
Alan-Miller <alan.ashman@gmail.com>
2016-12-08 01:07:45 -0800

    hex to decimal

---
+++ apprentice/hex-to-decimal.js
@@ -0,0 +1,6 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  HEX TO DECIMAL
+  https://www.codewars.com/kata/hex-to-decimal/train/javascript
+
+  Write function hexToDec which converts hex number (given as a string) to decimal number.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
