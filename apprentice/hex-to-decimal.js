398ac2a2fd43fe8e8737a58837eeb2d8a427e688
Alan-Miller <alan.ashman@gmail.com>
2016-12-08 01:08:39 -0800

    hex to decimal solution

---
+++ apprentice/hex-to-decimal.js
@@ -0,0 +1,10 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  HEX TO DECIMAL
+  https://www.codewars.com/kata/hex-to-decimal/train/javascript
+
+  Write function hexToDec which converts hex number (given as a string) to decimal number.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function hexToDec(hexString){
+	return parseInt(hexString, 16);
+}
