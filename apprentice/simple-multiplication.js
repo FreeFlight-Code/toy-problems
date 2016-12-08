31dd3c4179a8d5ad80c56f6de65745363318197e
Alan-Miller <alan.ashman@gmail.com>
2016-12-08 01:10:30 -0800

    simple multi

---
+++ apprentice/simple-multiplication.js
@@ -0,0 +1,6 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SIMPLE MULTIPLICATION
+  https://www.codewars.com/kata/simple-multiplication/train/javascript
+
+  This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
