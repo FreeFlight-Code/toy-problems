000519d12aef0c5d633e5f0b18aeb72277860e29
Alan-Miller <alan.ashman@gmail.com>
2016-12-08 01:13:18 -0800

    remove exclamation marks

---
+++ apprentice/remove-exclamation-marks.js
@@ -0,0 +1,7 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  REMOVE EXCLAMATION MARKS
+  https://www.codewars.com/kata/remove-exclamation-marks/train/javascript
+
+
+  Write function removeExclamationMarks which removes all exclamation marks from a given string.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
