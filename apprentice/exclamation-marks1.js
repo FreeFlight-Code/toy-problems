14e27fbcaf36ae6328fee3c891d15beed5884e5d
Alan-Miller <alan.ashman@gmail.com>
2016-12-18 15:41:42 -0700

    exclamation marks 1

---
+++ apprentice/exclamation-marks1.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  EXCLAMATION MARKS SERIES #1
+  https://www.codewars.com/kata/exclamation-marks-series-number-1-remove-a-exclamation-mark-from-the-end-of-string
+
+  Description:
+
+  Remove a exclamation mark from the end of string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+  Examples
+
+  remove("Hi!") === "Hi"
+  remove("Hi!!!") === "Hi!!"
+  remove("!Hi") === "!Hi"
+  remove("!Hi!") === "!Hi"
+  remove("Hi! Hi!") === "Hi! Hi"
+  remove("Hi") === "Hi"
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
