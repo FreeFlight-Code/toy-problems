fd8c167560330f231a5a6ed61a86edb25938d8fb
Alan-Miller <alan.ashman@gmail.com>
2016-12-18 15:44:22 -0700

    exclamation marks 2

---
+++ apprentice/exclamation-marks2.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  EXCLAMATION MARKS SERIES #2
+  https://www.codewars.com/kata/exclamation-marks-series-number-2-remove-all-exclamation-marks-from-the-end-of-sentence
+
+  Description:
+
+  Remove all exclamation marks from the end of sentence.
+  Examples
+
+  remove("Hi!") === "Hi"
+  remove("Hi!!!") === "Hi"
+  remove("!Hi") === "!Hi"
+  remove("!Hi!") === "!Hi"
+  remove("Hi! Hi!") === "Hi! Hi"
+  remove("Hi") === "Hi"
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
