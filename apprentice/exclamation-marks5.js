6532a8946104503a821ea0e1e4ee325ae262d7e4
Alan-Miller <alan.ashman@gmail.com>
2016-12-18 16:38:22 -0700

    exclamation marks 5 solution

---
+++ apprentice/exclamation-marks5.js
@@ -0,0 +1,20 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  EXCLAMATION MARKS SERIES #5
+  https://www.codewars.com/kata/exclamation-marks-series-number-5-remove-all-exclamation-marks-from-the-end-of-words
+
+  Description:
+
+  Remove all exclamation marks from the end of words. Words are separated by spaces in the sentence.
+  Examples
+
+  remove("Hi!") === "Hi"
+  remove("Hi!!!") === "Hi"
+  remove("!Hi") === "!Hi"
+  remove("!Hi!") === "!Hi"
+  remove("Hi! Hi!") === "Hi Hi"
+  remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function remove(s){
+  return s.replace(/\b!+/g, '');
+}
