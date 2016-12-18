84099c8c50862d5d55f8cbc94bbc49c47e82b6f1
Alan-Miller <alan.ashman@gmail.com>
2016-12-18 16:55:18 -0700

    exclamation marks 13

---
+++ journeyman/exclamation-marks13.js
@@ -0,0 +1,19 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  EXCLAMATION MARKS SERIES #13
+  https://www.codewars.com/kata/exclamation-marks-series-number-13-count-the-number-of-exclamation-marks-and-question-marks-return-the-product/train/javascript
+
+  Description:
+
+  Count the number of exclamation marks and question marks, return the product. The string only contains ! and ?.
+  Examples
+
+  product("") === 0
+  product("!") === 0
+  product("!!") === 0
+  product("!??") === 2
+  product("!???") === 3
+  product("!!!??") === 6
+  product("!!!???") === 9
+  product("!???!!") === 9
+  product("!????!!!?") === 20
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
