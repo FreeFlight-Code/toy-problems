7ca225586faeaa13474f8d490dc73232141236e6
Alan-Miller <alan.ashman@gmail.com>
2016-12-18 15:55:07 -0700

    exclamation marks 3

---
+++ apprentice/exclamation-marks3.js
@@ -0,0 +1,17 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  EXCLAMATION MARKS SERIES #3
+  https://www.codewars.com/kata/exclamation-marks-series-number-3-remove-all-exclamation-marks-from-sentence-except-at-the-end
+
+  Description:
+
+  Remove all exclamation marks from sentence except at the end.
+
+  Examples
+
+  remove("Hi!") == "Hi!"
+  remove("Hi!!!") == "Hi!!!"
+  remove("!Hi") == "Hi"
+  remove("!Hi!") == "Hi!"
+  remove("Hi! Hi!") == "Hi Hi!"
+  remove("Hi") == "Hi"
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
