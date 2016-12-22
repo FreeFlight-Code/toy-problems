8dd2cc7d69768b722e159784838d0f43e4cb4f13
Alan-Miller <alan.ashman@gmail.com>
2016-12-22 02:35:56 -0800

    roman numerals decoder solution

--- wizard/roman-numerals-decoder.js
+++ wizard/roman-numerals-decoder.js
@@ -11,3 +11,25 @@
   solution('XXI'); // should return 21
   C# RomanDecode.Solution("XXI") -- should return 21
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function solution(roman) {
+  // Key
+  var ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+  var tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+  var hund = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+  var thou = ['', 'M', 'MM', 'MMM'];
+  var search = ones.concat(tens).concat(hund).concat(thou);
+
+  // Transform into number
+  var numerals = roman.split('').map(function(c, i) {
+    var place = Math.floor(search.indexOf(c) / 10);
+    var digit = search.indexOf(c) % 10;
+    return Math.pow(10, place) * digit;
+  });
+  // Roman numerals subtract smaller amounts to the left of larger amounts
+  numerals = numerals.map(function(c, i, a) {
+    return i < a.length - 1 && c < numerals[i + 1] ? c * -1 : c;
+  });
+  // Add up the values
+  return numerals.reduce(function(a, b) {return a + b;})
+}
