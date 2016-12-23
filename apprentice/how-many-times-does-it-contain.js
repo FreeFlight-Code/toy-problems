0fd845b200e4bc5bfb1282a2f76ee416e89b5bfb
Alan-Miller <alan.ashman@gmail.com>
2016-12-22 23:45:23 -0800

    how many times solution

---
+++ apprentice/how-many-times-does-it-contain.js
@@ -0,0 +1,14 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  HOW MANY TIMES DOES IT CONTAIN
+  https://www.codewars.com/kata/how-many-times-does-it-contain/train/javascript
+
+  Your task is to return how many times a string contains a given character.
+
+  The function takes a string(inputS) as a paremeter and a char(charS) which is the character that you will have to find and count.
+
+  For example, if you get an input string "Hello world" and the character to find is "o", return 2.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function stringCounter(inputS, charS){
+  return inputS.split('').filter(function(c) {return c === charS;}).length;
+}
