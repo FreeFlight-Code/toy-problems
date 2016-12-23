1ba888d4da64664d16d65d223137335f3a07f8a3
Alan-Miller <alan.ashman@gmail.com>
2016-12-22 23:56:24 -0800

    kebabize

---
+++ apprentice/kebabize.js
@@ -0,0 +1,10 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  KEBABIZE
+  https://www.codewars.com/kata/kebabize/train/javascript
+
+  Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+  kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+  kebabize('camelsHave3Humps') // camels-have-humps
+  Notes: - the returned string should only contain lowercase letters
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
