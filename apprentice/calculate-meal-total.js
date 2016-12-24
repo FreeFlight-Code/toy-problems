d070a17988872f793ce618f3db2686c5018effc9
Alan-Miller <alan.ashman@gmail.com>
2016-12-23 23:58:47 -0800

    calculate meal total

---
+++ apprentice/calculate-meal-total.js
@@ -0,0 +1,8 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  CALCULATE MEAL TOTAL
+  https://www.codewars.com/kata/calculate-meal-total/train/javascript
+
+  Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
+
+  You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your result to two decimal places.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
