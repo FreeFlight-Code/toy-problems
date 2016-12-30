2ecfe8dc06c2c340af26e44db94d9b1911ac8b0b
Alan-Miller <alan.ashman@gmail.com>
2016-12-30 01:25:47 -0800

    all star code challenge 1

---
+++ apprentice/all-star-code-challenge1.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  ALL STAR CODE CHALLENGE #1
+  https://www.codewars.com/kata/all-star-code-challenge-number-1/train/javascript
+
+  This Kata is intended as a small challenge for my students
+
+  All Star Code Challenge #1
+
+  Write a function, called sumPPG, that takes two NBA player objects and sums their PPG
+
+  Examples:
+
+  var iverson = { team: '76ers', ppg: 11.2 };
+  var jordan =  { team: 'bulls', ppg: 20.2 };
+  sumPPG(iverson, jordan); // => 31.4
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
