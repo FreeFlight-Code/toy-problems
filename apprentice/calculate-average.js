0b6483b99a96ef47be36b6a5a9cdfbb629ff619c
Alan-Miller <alan.ashman@gmail.com>
2017-01-04 23:47:50 -0800

    highest and lowest

---
+++ apprentice/calculate-average.js
@@ -0,0 +1,6 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  CALCULATE AVERAGE
+  https://www.codewars.com/kata/calculate-average/train/javascript
+
+  Write function avg which calaculates average of numbers in given list.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
---
+++ journeyman/reverse-vowels-in-a-string.js
@@ -0,0 +1,10 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  REVERSE VOWELS IN A STRING
+  https://www.codewars.com/kata/reverse-vowels-in-a-string/train/javascript
+
+  In this kata, your goal is to write a function which will reverse the vowels in a string. Any characters which are not vowels should remain in their original position. Here are some examples:
+
+  reverseVowels("Hello!"); // "Holle!"
+  reverseVowels("Tomatoes"); // "Temotaos"
+  reverseVowels("Reverse Vowels In A String") // "RivArsI Vewols en e Streng"
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
