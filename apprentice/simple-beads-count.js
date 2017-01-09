ab713289de898a422bf2cf87bcb3aba586a64e4e
Alan-Miller <alan.ashman@gmail.com>
2017-01-08 23:58:02 -0800

    simple beads count

---
+++ apprentice/simple-beads-count.js
@@ -0,0 +1,11 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  SIMPLE BEADS COUNT
+  https://www.codewars.com/kata/simple-beads-count/train/javascript
+
+  Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+  @ @@ @ @@ @ @@ @ @@ @ @@ @
+
+  Implement count_red_beads(N_blue) (in Javascript countRedBeads(n)) so that it returns the number of red beads.
+  If there are less than 2 blue beads return 0.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
