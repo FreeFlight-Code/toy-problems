41d0a56804f63ffff5bbdc4a4a2e44f4c998d631
Alan-Miller <alan.ashman@gmail.com>
2017-07-26 01:51:38 -0600

    alternate problem

---
+++ apprentice/are-we-alternate.js
@@ -0,0 +1,13 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+    ARE WE ALTERNATE
+    https://www.codewars.com/kata/are-we-alternate/train/javascript
+
+    Create a function isAlt() that accepts a string as an argument and validates whether the vowels (a, e, i, o, u) and consonants are in alternate order.
+
+    isAlt("amazon")
+    // true
+    isAlt("apple")
+    // false
+    isAlt("banana")
+    // true
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
