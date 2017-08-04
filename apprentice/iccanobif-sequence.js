8a22ff0f3ddb9c6a5f98c3a4ff5a669e63b0f5ff
Alan-Miller <alan.ashman@gmail.com>
2017-08-04 03:44:13 -0600

    iccanobif

---
+++ apprentice/iccanobif-sequence.js
@@ -0,0 +1,12 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+    THE ICCANOBIF SEQUENCE
+    https://www.codewars.com/kata/the-iccanobif-sequence/train/javascript
+
+    Your task is to create an array of size n with the values of the Fibonnaci sequence, and reverse the order in which the sequence is displayed.
+
+    For example: [1, 1, 2, 3, 5] would become [5, 3, 2, 1, 1]
+
+    You can assume that n will always be a positive integer between and including, 1 and 64.
+
+    Note: If you receive a server timeout - Check your code and re-run the tests.
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
