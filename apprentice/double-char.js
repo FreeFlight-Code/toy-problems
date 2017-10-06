aca76e21be57784f17bb4cab58f94e298b09bf76
Alan-Miller <alan.ashman@gmail.com>
2017-10-05 23:53:22 -0600

    fizz-buzz and double-char

---
+++ apprentice/double-char.js
@@ -0,0 +1,16 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  DOUBLE CHAR
+  https://www.codewars.com/kata/double-char/train/javascript
+
+  Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+  doubleChar("String") ==> "SSttrriinngg"
+  doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+  doubleChar("1234!_ ") ==> "11223344!!__  "
+  
+  Good Luck!
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function doubleChar(str) {
+  // Your code here
+}
