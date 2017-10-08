d436ea8c9cfce1dc8cca95c5f9398e733b7cd560
Alan-Miller <alan.ashman@gmail.com>
2017-10-08 00:42:00 -0600

    roman numerals helper

---
+++ journeyman/roman-numerals-helper.js
@@ -0,0 +1,28 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+  ROMAN NUMERALS HELPER
+  https://www.codewars.com/kata/roman-numerals-helper
+
+  Create a RomanNumerals helper that can convert a roman numeral 
+  to and from an integer value. The class should follow the API 
+  demonstrated in the examples below. Multiple roman numeral 
+  values will be tested for each helper method.
+
+  Modern Roman numerals are written by expressing each digit 
+  separately starting with the left most digit and skipping any 
+  digit with a value of zero. In Roman numerals 1990 is rendered: 
+  1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 
+  2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in 
+  descending order: MDCLXVI.
+
+  Examples:
+
+  RomanNumerals.toRoman(1000); // should return 'M'
+  RomanNumerals.fromRoman('M'); // should return 1000
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+RomanNumerals = new Object();
+
+RomanNumerals.prototype.toRoman = function(x) {
+  console.log('hi');
+  return x;
+};
